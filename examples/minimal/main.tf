1394e6ad6bc6d98bcd00a0cdf4860f0675d63dee
Anton <patsev.anton@gmail.com>
2025-10-08 18:25:06 +0600

    add storage_buckets

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,3 +1,9 @@
+module "storage_buckets" {
+  source = "git::https://github.com/terraform-yacloud-modules/terraform-yandex-storage-bucket.git?ref=v1.0.0"
+
+  bucket_name = "my-unique-bucket-name"
+}
+
 module "minimal-example" {
   source = "../.." // Укажите путь к директории с модулем
 
@@ -5,7 +11,6 @@ module "minimal-example" {
 
   # ОБЯЗАТЕЛЬНО: Передайте статические ключи доступа
   # Рекомендуется хранить их в секретах (Vault, Yandex Lockbox) и передавать через переменные окружения TF_VAR_
-  access_key = "YC_access_key"
-  secret_key = "YC_secret_key"
-
+  access_key = module.storage_buckets.storage_admin_access_key
+  secret_key = module.storage_buckets.storage_admin_secret_key
 }
